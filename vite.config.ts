424bcbf8da54869bb239db185b2ec0b7df407c05
cibi0404 <fathima.offical.msg@gmail.com>
2024-06-24 17:46:19 +0530

    commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   build: {
     outDir: 'build'
-  }
-})
+  },
+  server: {
+    // Ensure the 'public' directory is served properly
+    open: true
+  },
+  // Ensure correct base path for the project
+  base: '/',
+});
