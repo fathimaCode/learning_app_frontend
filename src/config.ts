8a6dd79e71cc6bc2e8ea7122c0e7c7426e6a8534
cibi0404 <fathima.offical.msg@gmail.com>
2024-06-24 19:13:13 +0530

    update config file

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,5 @@
-const BASE_URL= "https://lms-backend-6nx9.onrender.com/"
+//"https://lms-backend-6nx9.onrender.com/"
+
+
+const BASE_URL= "https://lms-backend-6nx9.onrender.com"
 export default BASE_URL
